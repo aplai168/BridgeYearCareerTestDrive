3abb65bdf47319975e7257c45d26a6929ca8890f
Alice Lai <aplai168@gmail.com>
2018-06-24 17:10:47 -0500

    embedded ace editor -Alice

--- templates/main_panel/web.php
+++ templates/main_panel/web.php
@@ -1,6 +1,7 @@
 <div class = "container" style = "padding-top: 40px;">
 	<div class = "row">
-		<div class = "col-md-12">
+		<!-- <div class = "col-md-12"> -->
+		<div class = "col-sm-4">
 			<div class = "panel panel-default" id = "questionPortal">
 				<div class = "panel-heading">
 					<h2 style = "text-align: center;">Question</h2>
@@ -62,7 +63,7 @@
 						<button class = "btn btn-default" id = "startTask">Start Task</button>
 					</span>
 					<span>
-						
+
 						<button class = "btn btn-default" id = "q1respbutton" style = "display: none;">Next Question</button>
 						<button class = "btn btn-default" id = "q2respbutton" style = "display: none;">Next Question</button>
 						<button class = "btn btn-default" id = "q3respbutton" style = "display: none;">Next Question</button>
@@ -141,7 +142,7 @@
 						</div>
 					</div>
 				</div>
-			</div>	
+			</div>
 		</div>
 		<div class = "col-md-6">
 			<textarea class = "form-control" id = "formCode" style = "height: 400px;">
@@ -206,20 +207,100 @@
 					</div>
 				</div>
 			</textarea>
-			<div class = "row">
-				<div class = "col-md-4">
-					<button class = "btn btn-primary" id = "refreshCode">Update Code</button>
+			<div id="editor">
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+	&lt;script&gt;
+	    function sayHello() {
+	        console.log('hello')
+	    }
+	&lt;/script&gt;
+    &lt;body&gt;
+		&lt;button onclick="sayHello()"&gt;Say Hello&lt;/button&gt;
+		&lt;div class = "panel panel-default"&gt;
+		  &lt;div class = "panel-body"&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Password&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "password1" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Favorite Number&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "favenumber" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Favorite Color&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "favecolor" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Birthday&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "date" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Do you like this CTD?&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "question" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;Rate this CTD on a scale of one to ten.&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "rating" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		    &lt;div class = "row"&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;h2&gt;What time is it?&lt;/h2&gt;
+		      &lt;/div&gt;
+		      &lt;div class = "col-md-6"&gt;
+		        &lt;input type = "text" id = "time" class = "form-control"&gt;
+		      &lt;/div&gt;
+		    &lt;/div&gt;
+		  &lt;/div&gt;
+		&lt;/div&gt;
+	&lt;/body&gt;
+&lt;/html&gt;
 				</div>
-				<div class = "col-md-4">
-					<button class = "btn btn-primary" id = "resetCode">Reset Code</button>
-				</div>
+		</div>
+		<div class = "row">
+			<div class = "col-md-4">
+				<button class = "btn btn-primary" id = "refreshCode">Update Code test5</button>
+			</div>
+			<div class = "col-md-4">
+				<button class = "btn btn-primary" id = "resetCode">Reset Code test5</button>
 			</div>
 		</div>
 	</div>
-	
-</div>
-<script type="text/javascript">
 
+</div>
+
+<script src="/ace-builds/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+	var editor = ace.edit("editor");
+	var code = editor.getValue();
+	console.log(code)
+	editor.setTheme("ace/theme/monokai");
+	editor.session.setMode("ace/mode/html");
+	// editor.session.setMode("ace/mode/javascript");
  	function User() {
  		this.code = '<div class = "panel panel-default">\
 			<div class = "panel-body">\
@@ -286,27 +367,29 @@
 	var newUser = new User();
 
 	User.prototype.update = function(){
-		alert(document.getElementById('formCode').value);
-		this.code = document.getElementById('formCode').value;
+		alert(document.getElementById('editor').value);
+		this.code = document.getElementById('editor').value;
 		alert(this.code);
 	}
 
 	User.prototype.resetCode = function(){
 		alert(this.code);
-		document.getElementById('formCode').value = this.code;
-		document.getElementById('textform').innerHTML = document.getElementById('formCode').value;
+		document.getElementById('editor').value = this.code;
+		document.getElementById('textform').innerHTML = document.getElementById('editor').value;
 	}
 
 	document.getElementById('refreshCode').addEventListener('click',function(){
-		document.getElementById('textform').innerHTML = document.getElementById('formCode').value;
-		alert('Code has been updated');
+		const editor = ace.edit('editor');
+		const code = editor.getValue();
+		$('#textform').html(code);
+		eval(code);
 	});
-	
+
 	document.getElementById('resetCode').addEventListener('click', function(){
 		document.getElementById('formCode').value = newUser.code;
 		document.getElementById('textform').innerHTML = document.getElementById('formCode').value;
 	});
-	
+
 	document.getElementById('startTask').addEventListener('click',function(){
 		document.getElementById('q1respbutton').style.display = 'block';
 		document.getElementById('q1resp').style.display = 'block';
@@ -340,7 +423,7 @@
 		document.getElementById('q4respbutton').style.display = 'none';
 		document.getElementById('q4resp').style.display = 'none';
 	});
-	
+
 	document.getElementById('q5respbutton').addEventListener('click',function(){
 		document.getElementById('q6respbutton').style.display = 'block';
 		document.getElementById('q6resp').style.display = 'block';
@@ -362,3 +445,12 @@
 		document.getElementById('q7resp').style.display = 'none';
 	});
 </script>
+<style type="text/css" media="screen">
+    #editor {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+</style>
